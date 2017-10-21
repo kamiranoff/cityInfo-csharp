4ccb899f930e7825c5c996e2a198610641da174a
Kevin Amiranoff <kamiranoff@gmail.com>
2017-10-21 19:01:09 +0100

    Adds commented out .AddJsonOptions, in case of need of manipulation json.

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfo.API
 {
@@ -17,7 +18,13 @@ namespace CityInfo.API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddMvc();
+            services.AddMvc()
+                    .AddJsonOptions(o => {
+                if(o.SerializerSettings.ContractResolver != null) {
+                            var castedResoler = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+                            castedResoler.NamingStrategy = null;
+                }
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
